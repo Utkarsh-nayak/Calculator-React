04686825c9032cd649c61916d6d129016ae1415e
Utkarsh-nayak <utkarshnayak1714@gmail.com>
2025-01-17 01:46:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Calculator from './components/Calculator'
+
+const App = () => {
+  return (
+    <div>
+<Calculator/>
+
+
+
+
+    </div>
+  )
+}
+
+export default App
